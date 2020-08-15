e0d30f38850e83938fc31a89565e00d7094b2197
Tetiana <bachurinat@gmail.com>
2020-08-15 21:45:30 +0300

    task-3 done

--- task-3.js
+++ task-3.js
@@ -1,7 +1,5 @@
-const userPassword = 'jqueryismyjam';
-
+const userPassword = prompt('Введите пароль');
 const ADMIN_PASSWORD = 'jqueryismyjam';
-
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
 const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
@@ -9,10 +7,12 @@ let message;
 
 if (userPassword === ADMIN_PASSWORD) {
   message = ACCESS_IS_ALLOWED;
-} else if 
-  (userPassword === null) {
+  alert(message);
+} else if (userPassword === null) {
   message = CANCELED_BY_USER;
+  alert(message);
 } else {
   message = ACCESS_DENIED;
+  alert(message);
 }
 console.log(message);
