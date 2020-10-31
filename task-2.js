8c3efb7eb914b15a81136ad1067606c3c9b98b89
Tetiana <bachurinat@gmail.com>
2020-10-31 11:33:13 +0200

    js-hw1

--- task-2.js
+++ task-2.js
@@ -1,8 +1,17 @@
-const total = 100;
-const order = 20;
+// const total = 100;
+// const order = 20;
 
+// const message =
+//   order <= total
+//     ? 'Заказ оформлен, с вами свяжется менеджер'
+//     : 'На складе недостаточно товаров!';
+// console.log(message);
+const invoice = 100; 
+const stock = 100;
+
+// Write code under this line
 const message =
-  order <= total
+  invoice <= stok
     ? 'Заказ оформлен, с вами свяжется менеджер'
     : 'На складе недостаточно товаров!';
 console.log(message);
